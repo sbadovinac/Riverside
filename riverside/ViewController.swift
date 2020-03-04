16783bda8300fe753f2b07f60370b203a69b83d5
Sam Badovinac <sam.badovinac@gmail.com>
2020-03-04 16:45:31 -0500

    inital commit

---
+++ riverside/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  riverside
+//
+//  Created by Badovinac, Sam on 3/4/20.
+//  Copyright © 2020 EPICS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
